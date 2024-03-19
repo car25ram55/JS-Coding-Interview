aba6aa7d30e61c9c19468ab9541dc287af096ecf
camo55 <car25ram@gmail.com>
2024-03-19 16:31:53 -0400

    newCommit

--- reverse.js
+++ reverse.js
@@ -4,18 +4,46 @@
 // reverse('hello') === 'olleh'
 // reverse('CodingMoney') === 'yenoMgnidoC'
 
+// function reverse(str) {
+//   let reversed = " ";
+//   for (let i = 0; i < str.length; i++) {
+//     reversed = str[i] + reversed;
+//   }
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   let reversed = " ";
+
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// Reverses the order of words in a string
+// function reverse(str) {
+//   const strToArray = str.split(" ");
+
+//   strToArray.reverse();
+
+//   return strToArray.join(" ");
+// }
+
+// Reverses the order of words in a string
+// function reverse(str) {
+//   return str.split(" ").reverse().join(" ");
+// }
+
 function reverse(str) {
-  let reversed = "";
-
-  for (let char of str) {
-    reversed = char + reversed;
-  }
-
-  return reversed;
+  const strToArray = str.split(" ");
+  const reversed = strToArray.map((word) => word.split("").reverse().join(""));
+  return reversed.join(" ");
 }
 
-console.log(reverse("CodingMoney"));
+console.log(reverse("Coding Money"));
 
-console.log(reverse("Hello"));
+console.log(reverse("Hello Man"));
 
-console.log(reverse("Hi!"));
+console.log(reverse("Hi Dad!"));
